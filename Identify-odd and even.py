112a74ad116e7884c1b6fdc119a77a47892c9e69
lemuela1 <107967363+lemuela1@users.noreply.github.com>
2022-06-25 21:27:01 -0500

    Add files via upload

---
+++ Identify-odd and even.py
@@ -0,0 +1,9 @@
+""" This is a program for identifying odd and even numbers."""
+while 1 > 0:
+    #x = int(input("请输入数字"))
+    #if x%2 == 0:
+        #print("此数为偶数")
+    #else:
+        #print("此数是奇数")
+    x = int(input("Please write down a number:"))
+    print("此数是偶数") if x%2 == 0 else print("此数是奇数")
